76746c55cc9bd5d29777c8a0855adb7d77ed5cd6
kazemicode <sarieses@gmail.com>
2019-10-23 01:17:29 -0700

    sumNine header file for example 13.3.1

---
+++ Assignments/A6/sumNine1.h
@@ -0,0 +1,9 @@
+/* sumNine1.h
+ * Computes sum of nine integers.
+ * 2017-09-29: Bob Plantz
+ */
+#ifndef SUMNINE_H
+#define SUMNINE_H
+int sumNine(int one, int two, int three, int four, int five,
+           int six, int seven, int eight, int nine);
+#endif
