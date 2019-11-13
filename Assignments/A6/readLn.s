f6e896351056a91907b41f3adeb3862c65708ed2
kazemicode <sarieses@gmail.com>
2019-11-12 16:08:59 -0800

    Add readLn modified for max chars

--- Assignments/A6/readLn.s
+++ Assignments/A6/readLn.s
@@ -26,14 +26,17 @@
     .type   readLn, %function
 
 readLn:
-    sub   sp, sp, 16    @ space for saving registers
-    str   r4, [sp, 0]   @ save r4 to stack
-    str   r5, [sp, 4]   @ save r5 to stack
-    str   fp, [sp, 8]   @ save fp to stack
-    str   lr, [sp, 12]  @ save lr to stack
-    add   fp, sp, 12    @ set frame pointer
+    sub   sp, sp, 24    @ space for saving registers
+    str   r4, [sp, 4]   @ save r4 to stack
+    str   r5, [sp, 8]   @ save r5 to stack
+    str   r6, [sp, 12]  @ save r6 to stack
+    str   fp, [sp, 16]   @ save fp to stack
+    str   lr, [sp, 24]  @ save lr to stack
+    add   fp, sp, 20    @ set frame pointer
 
     mov   r4, r0        @ r4 = string pointer
+    mov   r6, r1        @ r6 = max chars
+    sub   r6, 1         @ for NUL, max char - 1
     mov   r5, 0         @ r5 = counter variable
 
     mov   r0, STDIN     @ read from keyboard
@@ -47,6 +50,9 @@ whileLoop:
     cmp   r3, LF        @ End of input?
     beq   allDone       @ Yes? new line, done.
 
+    cmp   r5, r6        @ max chars?
+    bge   ignore        @ yes, ignore rest of chars
+
     add   r4, r4, 1     @ No? Increment pointer var
     add   r5, r5, 1     @ Increment counter var
 
@@ -55,14 +61,22 @@ whileLoop:
     bl    read
     b     whileLoop     @ Loop again
 
+ignore:
+    mov   r0, STDIN     @ Read from keyboard
+    mov   r1, r4        @ address of current storage
+    mov   r2, 1         @ read 1 byte
+    bl    read
+    b     whileLoop     @ check for end
+
 
 allDone:
     mov   r0, NUL       @ string terminator
     strb  r0, [r4]      @ overwrite newline with NUL
     mov   r0, r5        @ return count of characters stored
-    ldr   r4, [sp, 0]   @ restore r4
-    ldr   r5, [sp, 4]   @ restore r5
-    ldr   fp, [sp, 8]   @ restore fp
-    ldr   lr, [sp, 12]  @ restore lr
-    add   sp, sp, 16    @ restore sp
+    ldr   r4, [sp, 4]   @ restore r4
+    ldr   r5, [sp, 8]   @ restore r5
+    ldr   r5, [sp, 12]   @ restore r6
+    ldr   fp, [sp, 16]   @ restore fp
+    ldr   lr, [sp, 24]  @ restore lr
+    add   sp, sp, 24    @ restore sp
     bx    lr            @ return
---
+++ Assignments/A6/readLnTest2.s
@@ -0,0 +1,59 @@
+@ writeLnTest2.s
+@ Tests readLn by echoing a string the user inputs
+@ This time, though, an string has a max size
+@ Sara Kazemi
+
+@ Define raspi
+    .cpu    cortex-a53
+    .fpu    neon-fp-armv8
+    .syntax unified
+
+@ Constant data
+    .equ      nBytes, 50 @ amount of memory for string
+    .section  .rodata
+    .align    2
+prompt:
+    .asciz    "Enter a string: \n"
+
+@ Code start
+    .text
+    .align  2
+    .global main
+    .type   main, %function
+
+main:
+    sub   sp, sp, 16    @ space for saving regs
+    str   r4, [sp, 4]   @ save r4 to stack
+    str   fp, [sp, 8]   @ save fp to stack
+    str   lr, [sp, 12]  @ save lr to stack
+    add   fp, sp, 12    @ set frame pointer
+
+    mov r0, nBytes      @ memory from heap
+    bl  malloc          @ memory allocation
+    mov r4, r0          @ pointer to new memory
+
+    ldr   r0, promptAddr    @ prompt user
+    bl    writeStr          @ call function
+
+    @ After writeStr finished executing
+    mov   r0, r4        @ get user input
+    mov   r1, nBytes    @ LIMIT INPUT SIZE
+    bl    readLn
+
+    @ After readLn finished executing
+    mov   r0, r4            @ echo user input
+    bl    writeStr          @ call function
+
+    @ After second call to writeStr finished executing
+    mov   r0, r4        @ free heap memory
+    bl    free
+
+    mov   r0, 0         @ return 0
+    ldr   r4, [sp, 4]   @ restore r4
+    ldr   fp, [sp, 8]   @ restore caller fp
+    ldr   lr, [sp, 12]  @ restore lr
+    add   sp, sp, 16    @ restore sp
+    bx    lr            @ return
+
+promptAddr:
+    .word   prompt    @ address of myString
