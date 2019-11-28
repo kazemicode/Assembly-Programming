f20eaeea7ccaa8a11f9ee8233c8e4730997fafc2
kazemicode <sarieses@gmail.com>
2019-11-27 16:24:30 -0800

    lab3

---
+++ Labs/Lab 3/gcd/factiorial.s
@@ -0,0 +1,51 @@
+@ Define my Raspberry pi
+.cpu cortex-a53
+.fpu neon-fp-armv8
+.syntax unified
+.data
+output: .asciz "Factorial of %d is: %d"
+omsg: .asciz "Overflow occurred."
+
+
+@ Program code
+.text
+.align 2
+.global factorial
+.type factorial, %function
+
+factorial:
+
+push {fp, lr}
+add fp, sp, #4
+
+@ r4 has user input (n)
+@ r5 will be final value
+@ r6 untouched user input
+
+mov r5, r6
+mov r5, r4
+b loop
+
+loop:
+cmp r4, #1
+beq done
+sub r4, r4, #1
+umull r5, r0, r4, r5
+cmp r0, #1
+beq overflow
+b loop
+
+
+done:
+mov r1, r6
+mov r2, r5
+ldr r0, =output
+bl printf
+sub sp, fp, #4
+pop {fp, pc}
+
+overflow:
+ldr r0, =omsg
+bl printf
+sub sp, fp, #4
+pop {fp, pc}
--- Labs/Lab 3/gcd/main.s
+++ Labs/Lab 3/gcd/main.s
@@ -3,8 +3,7 @@
 .fpu neon-fp-armv8
 .syntax unified
 .data
-prompt1: .asciz "Enter first positive integer: "
-prompt2: .asciz "Enter second positive integer: "
+prompt: .asciz "Enter an integer: "
 input: .asciz "%d"
 
 @ Program code
@@ -19,7 +18,7 @@ push {fp, lr}
 add fp, sp, #4
 @ print prompt for user
 mov r10, lr
-ldr r0, =prompt1
+ldr r0, =prompt
 bl printf
 
 @ call scanf to read user input
@@ -31,18 +30,8 @@ bl scanf @ sp contain the input
 @ inp stored in r4
 ldr r4, [sp]
 
-ldr r0, =prompt2
-bl printf
-
-@ call scanf to read user input
-sub sp, sp, #4
-ldr r0, =input
-mov r1, sp
-bl scanf @ sp contain the input
-@ inp stored in r5
-ldr r5, [sp]
 
 
-bl gcd
+bl factorial
 sub sp, fp, #4
 pop {fp, pc}
