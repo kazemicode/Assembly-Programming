6929bf25dda21a76764bf80d370f5a795e7d1079
kazemicode <sarieses@gmail.com>
2019-11-27 17:46:45 -0800

    add makefiles for lab3

---
+++ Labs/Lab 4/checkPrimeNumber.s
@@ -0,0 +1,3 @@
+@ n will be in register set in main
+@ j is a divisor used to determine if n is Prime
+@ flag is set to 0 if not prime, 1 if prime (return value)
---
+++ Labs/Lab 4/main.s
@@ -0,0 +1,43 @@
+@ need two inputs
+@ counter var for loop
+@ prime number flag
+
+
+@ Define my Raspberry pi
+.cpu cortex-a53
+.fpu neon-fp-armv8
+.syntax unified
+.data
+prompt1: .asciz "Enter first integer: "
+input: .asciz "%d"
+prompt2: .asciz "Enter second integer: "
+
+@ Program code
+.text
+.align 2
+.global main
+.type main, %function
+
+main:
+
+push {fp, lr}
+add fp, sp, #4
+@ print prompt for user
+mov r10, lr
+ldr r0, =prompt
+bl printf
+
+@ call scanf to read user input
+sub sp, sp, #4
+ldr r0, =input
+mov r1, sp
+bl scanf @ sp contain the input
+
+@ inp stored in r4
+ldr r4, [sp]
+
+
+
+bl factorial
+sub sp, fp, #4
+pop {fp, pc}
