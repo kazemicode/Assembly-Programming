a312aa3029b4684a9931992ba7ef52160f27ad20
kazemicode <sarieses@gmail.com>
2019-12-11 23:00:12 -0800

    add testing dir

--- Labs/testing/main.s
+++ Labs/testing/main.s
@@ -38,9 +38,9 @@ ldr r4, [sp]
 ldr r10, =#10000  @ used for mod for isolating digits
 mov r2, #10
 
-loop:
-cmp r10, #1    @ check if we've iterated over all digits
-beq done       @ if yes, we're done
+@loop:
+@cmp r10, #1    @ check if we've iterated over all digits
+@beq done       @ if yes, we're done
 
 @ MOD to isolate digits
 udiv r7, r4, r10  @ 95823 / 10000 = 9
@@ -50,7 +50,7 @@ sub r6, r4, r8  @ 95823 - 90000 = 5823
 udiv r10, r10, r2  @ move to next mod for next digit
 
 bl bar          @ convert current digit to barcode equivalent
-b loop          @ check loop condition again
+@b loop          @ check loop condition again
 
 
 done:
